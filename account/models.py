16139bbdef1044fbe6f0f41497b2bc5d40698c03
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-09-11 18:04:47 +0600

    initial user model

---
+++ account/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+from core.models import BaseModel
+
+
+class UserType(models.TextChoices):
+    ADMIN = 'ADMIN', _('Admin')
+    EMPLOYEE = 'EMPLOYEE', _('Employee')
+    OWNER = 'OWNER', _('Owner')
+
+
+class User(AbstractUser, BaseModel):
+    user_types = models.CharField(
+        max_length=10,
+        choices=UserType.choices,
+        default=UserType.EMPLOYEE,
+    )
+
+    @property
+    def token(self):
+        return ''
