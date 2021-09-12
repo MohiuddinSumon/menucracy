83a828a0b25caa44ac75860a79ce65a7490a192c
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-09-12 16:23:40 +0600

    3 day winner check

--- vote/views.py
+++ vote/views.py
@@ -1,8 +1,11 @@
+from datetime import date
+from datetime import timedelta
+
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 
-from restaurant.models import Menu
+from restaurant.models import Menu, Restaurant
 from restaurant.serializers import ReadMenuSerializer
 from vote.serializers import VoteSerializer
 
@@ -13,8 +16,27 @@ class VoteViewSet(ModelViewSet):
 
 class WinnerMenu(APIView):
 
-    def get(self, request):
-        menu = Menu.objects.select_related('restaurant').all().order_by('-vote_count').first()
+    @staticmethod
+    def get_winner(day):
+        return Menu.objects.select_related('restaurant').filter(serving_date=day).order_by('-vote_count').first()
 
-        serializer = ReadMenuSerializer(menu)
+    def get(self, request):
+
+        today = date.today()
+        yesterday = today - timedelta(days=1)
+        day_before_yesterday = today - timedelta(days=2)
+
+        winner = self.get_winner(today)
+
+        if Restaurant.objects.count() > 1:
+            yesterday_winner = self.get_winner(yesterday)
+            day_before_yesterday_winner = self.get_winner(day_before_yesterday)
+
+            if winner and yesterday_winner and day_before_yesterday_winner:
+                if winner.restaurant == yesterday_winner.restaurant == day_before_yesterday_winner.restaurant:
+                    second_winner = Menu.objects.select_related('restaurant').filter(serving_date=today).order_by('-vote_count')[1]
+                    serializer = ReadMenuSerializer(second_winner)
+                    return Response(serializer.data)
+
+        serializer = ReadMenuSerializer(winner)
         return Response(serializer.data)
